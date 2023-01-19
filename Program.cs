4f7bb78443fc66c8b3df5c528ac6c815d0bf9724
EddieTavares <eddie.tavares@live.com>
2023-01-19 17:49:47 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe o seu nome e sobrenome:");
+            var nome = Console.ReadLine();
+
+            // Console.WriteLine("Informe a sua idade:");
+            // var idade = Console.ReadLine();
+
+            // Console.WriteLine("Informe o seu telefone:");
+            // var telefone = Console.ReadLine();
+
+            // (xx)99999-9999
+            // var telefone = "0123456789F";
+
+            // string telefoneFormat = 
+            //     'd' + telefone.Substring(0, 2) + 
+            //     ')' + telefone.Substring(2, 5) + 
+            //     '-' + telefone.Substring(7, 4);
+
+            // Console.WriteLine(telefoneFormat);
+            
+            var i = 100;
+            double d = Convert.ToDouble(i);
+
+
+
+
+
+
+        }
+    }
+}
